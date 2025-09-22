54ed19a37f2888c92dcdf20c70d0405aaa48e248
lolito996 <josealejandromc4@gmail.com>
2025-09-22 13:32:41 -0500

    feat: implement Cache Aside pattern with Redis for Users API

--- users-api/src/main/java/com/elgris/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/elgris/usersapi/UsersApiApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+@EnableCaching
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
